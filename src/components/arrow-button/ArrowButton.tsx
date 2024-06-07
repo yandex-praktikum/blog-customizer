56d8922efbfb45df242993d2e21843f2cbbacf1e
DreamHarakiri <derek.gaming744@gmail.com>
2024-06-07 22:06:51 +0300

    боже помоги ч1

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,36 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type OnClick = (isOpenForm: boolean) => void;
 
-export const ArrowButton = () => {
+export type TArrowButton = {
+	isOpenForm: boolean;
+	onClick: OnClick;
+};
+
+export const ArrowButton = ({ isOpenForm, onClick }: TArrowButton) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, {
+				[styles.container_open]: isOpenForm,
+			})}
+			onClick={() => {
+				onClick(!isOpenForm);
+			}}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, {
+					[styles.arrow_open]: isOpenForm,
+				})}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,19 +2,130 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState, useRef } from 'react';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+import { Text } from '../text';
+import { Select } from '../select';
+import {
+	ArticleStateType,
+	fontSizeOptions,
+	contentWidthArr,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	OptionType,
+	defaultArticleState,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+
+export type TArticleParamsForm = {
+	articleState: ArticleStateType;
+	onSubmitState: (articleState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	onSubmitState,
+}: TArticleParamsForm) => {
+	const [isOpenForm, setOpenForm] = useState<boolean>(false);
+	const [isOptionForm, setOptionForm] =
+		useState<ArticleStateType>(articleState);
+	const formRef = useRef<HTMLDivElement | null>(null);
+
+	const onSubmitForm = (e: React.FormEvent) => {
+		e.preventDefault();
+		onSubmitState(isOptionForm);
+	};
+
+	useOutsideClickClose({
+		rootRef: formRef,
+		isOpen: isOpenForm,
+		onChange: () => setOpenForm(false),
+	});
+
+	const onChangeSelected = (
+		key: keyof ArticleStateType,
+		option: OptionType
+	) => {
+		setOptionForm((prev) => ({ ...prev, [key]: option }));
+	};
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={formRef}>
+			<ArrowButton onClick={setOpenForm} isOpenForm={isOpenForm} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpenForm,
+				})}>
+				<form className={styles.form} onSubmit={onSubmitForm}>
+					<Text weight={800} size={31} align={'left'} uppercase={true}>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='шрифт'
+						options={fontFamilyOptions}
+						selected={isOptionForm.fontFamilyOption}
+						onChange={(isOptionForm) => {
+							onChangeSelected('fontFamilyOption', isOptionForm);
+						}}
+					/>
+
+					<RadioGroup
+						title='размер шрифта'
+						options={fontSizeOptions}
+						selected={isOptionForm.fontSizeOption}
+						name={'font-size'}
+						onChange={(isOptionForm) => {
+							onChangeSelected('fontSizeOption', isOptionForm);
+						}}
+					/>
+
+					<Select
+						title='цвет шрифта'
+						options={fontColors}
+						selected={isOptionForm.fontColor}
+						onChange={(isOptionForm) => {
+							onChangeSelected('fontColor', isOptionForm);
+						}}
+					/>
+
+					<Separator />
+
+					<Select
+						title='цвет фона'
+						options={backgroundColors}
+						selected={isOptionForm.backgroundColor}
+						onChange={(isOptionForm) => {
+							onChangeSelected('backgroundColor', isOptionForm);
+						}}
+					/>
+
+					<Select
+						title='ширина контента'
+						options={contentWidthArr}
+						selected={isOptionForm.contentWidth}
+						onChange={(isOptionForm) => {
+							onChangeSelected('contentWidth', isOptionForm);
+						}}
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button
+							title='Сбросить'
+							type='reset'
+							onClick={() => {
+								setOptionForm(defaultArticleState);
+								onSubmitState(defaultArticleState);
+							}}
+						/>
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
