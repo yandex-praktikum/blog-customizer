c5a3ad573e11533b8379b7a7e4fa872a2da03390
Natalia Grishina <agrishinstudio@yandex.ru>
2024-09-04 17:42:34 +0300

    open/close aside

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,15 +5,29 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	isVisible?: boolean;
+	onClick?: OnClick;
+}
+
+export const ArrowButton: React.FC<ArrowButtonProps> = ({
+	isVisible,
+	onClick,
+}) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={`${styles.container}
+			${isVisible ? styles.container_open : ''}`}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={`${styles.arrow} ${isVisible ? styles.arrow_open : ''}`}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,22 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isVisible, setIsVisible] = useState(true);
+
+	const toggleVisible = () => {
+		setIsVisible((prev) => !prev);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isVisible={isVisible} onClick={toggleVisible} />
+			<aside
+				className={`${styles.container} ${
+					isVisible ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
