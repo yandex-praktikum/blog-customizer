71e04f37ad6b2d1dce3dfeca0cba710be6adad45
zen <zen-ja@yandex.ru>
2026-08-30 14:13:06 +0300

    add: вариант 1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,34 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import { defaultArticleState, TAppState } from 'src/constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [appState, setAppState] = useState<TAppState>({
+		currentStyle: defaultArticleState,
+		setState: (state) => setAppState({ ...appState, currentStyle: state }),
+	});
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appState.currentStyle.fontFamilyOption.value,
+					'--font-size': appState.currentStyle.fontSizeOption.value,
+					'--font-color': appState.currentStyle.fontColor.value,
+					'--container-width': appState.currentStyle.contentWidth.value,
+					'--bg-color': appState.currentStyle.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				currentStyle={appState.currentStyle}
+				setState={appState.setState}
+			/>
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,103 @@
+import { useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
 
 import styles from './ArticleParamsForm.module.scss';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	TAppState,
+	OptionType,
+	ArticleStateType,
+	defaultArticleState,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm = (props: TAppState) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [state, setState] = useState(props.currentStyle);
+
+	function select(
+		title: string,
+		optionName: keyof ArticleStateType,
+		optionList: OptionType[]
+	) {
+		const onchange = (e: OptionType) => {
+			setState({
+				...state,
+				[optionName]: e,
+			});
+		};
+		return (
+			<Select
+				title={title}
+				selected={state[optionName]}
+				options={optionList}
+				onChange={onchange}
+				{...props}
+			/>
+		);
+	}
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setIsOpen((prev) => !prev);
+				}}
+			/>
+			<aside className={isOpen ? styles.container_open : styles.container}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+					}}>
+					<Text size={31} weight={800} uppercase>
+						задайте параметры
+					</Text>
+					{select('шрифт', 'fontFamilyOption', fontFamilyOptions)}
+					<RadioGroup
+						options={fontSizeOptions}
+						name='fontSize'
+						selected={state.fontSizeOption}
+						key={0}
+						onChange={(e) => {
+							setState({
+								...state,
+								fontSizeOption: e,
+							});
+						}}
+						title='размер шрифта'
+					/>
+					{select('цвет шрифта', 'fontColor', fontColors)}
+					<Separator />
+					{select('цвет фона', 'backgroundColor', backgroundColors)}
+					{select('ширина контента', 'contentWidth', contentWidthArr)}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => {
+								props.setState(defaultArticleState);
+							}}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={() => {
+								props.setState(state);
+								setIsOpen(false);
+							}}
+						/>
 					</div>
 				</form>
 			</aside>
